ce6cdc3548a92bcc024f2c1f423b9834aa5a877f
Ivan Bushchik <ivabus@ivabus.dev>
2022-07-09 17:46:01 +0300

    Move configs to `/configs`

--- tools/relink.sh
+++ tools/relink.sh
@@ -8,7 +8,7 @@ CLEAR_COLOR="\033[0m"
 
 mkdir -p $HOME/.config/nvim >/dev/null 2>&1
 rm $HOME/.zshrc $HOME/.config/nvim/init.vim >/dev/null 2>&1
-ln -s $HOME/.dotfiles/zshrc $HOME/.zshrc
-echo "${GREEN}.zshrc linked: $CLEAR_COLOR$HOME/.dotfiles/zshrc $GREEN-> $CLEAR_COLOR$HOME/.zshrc"
+ln -s $HOME/.dotfiles/configs/zshrc $HOME/.zshrc
+echo "${GREEN}.zshrc linked: $CLEAR_COLOR$HOME/.dotfiles/configs/zshrc $GREEN-> $CLEAR_COLOR$HOME/.zshrc"
 ln -s $HOME/.dotfiles/nvim.vim $HOME/.config/nvim/init.vim
-echo "${GREEN}neovim config linked: $CLEAR_COLOR$HOME/.dotfiles/nvim.vim $GREEN-> $CLEAR_COLOR$HOME/.config/nvim/init.vim"
+echo "${GREEN}neovim config linked: $CLEAR_COLOR$HOME/.dotfiles/configs/nvim.vim $GREEN-> $CLEAR_COLOR$HOME/.config/nvim/init.vim"
