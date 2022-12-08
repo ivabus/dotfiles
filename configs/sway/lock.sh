17117171fc3ffb32c12512edbeb1f53bfb1c1eda
Ivan Bushchik <ivabus@ivabus.dev>
2022-12-08 12:18:45 +0300

    Change theme to Catpputin Macchiato and some few more chages

--- configs/sway/lock.sh
+++ configs/sway/lock.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Times the screen off and puts it to background
 swayidle \
-    timeout 10 'swaymsg "output * dpms off"' \
-    resume 'swaymsg "output * dpms on"' &
+    timeout 10 'swaymsg "output * dpms off" && brightnessctl -d kbd_backlight set 0%' \
+    resume 'swaymsg "output * dpms on" && brightnessctl -d kbd_backlight set 30%' &
 # Locks the screen immediately
 swaylock -c 073642
 # Kills last background task so idle timer doesn't keep running
