dc3588e48ea4730c2e3db4e114c182cbdeacd481
Ivan Bushchik <ivabus@ivabus.dev>
2022-12-08 14:00:13 +0300

    Change backgounds scale to stretch and change lockscreen font to JB Mono

--- configs/sway/lock.sh
+++ configs/sway/lock.sh
@@ -4,6 +4,6 @@ swayidle \
     timeout 10 'swaymsg "output * dpms off" && brightnessctl -d kbd_backlight set 0%' \
     resume 'swaymsg "output * dpms on" && brightnessctl -d kbd_backlight set 30%' &
 # Locks the screen immediately
-swaylock -c 073642
+swaylock -i ~/.config/sway/magenta_blue.png -s stretch --font JetBrains\ Mono
 # Kills last background task so idle timer doesn't keep running
 kill %%
