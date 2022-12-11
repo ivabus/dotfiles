80f05c8a7407970c0ed4dda8326bd09900626b2b
Ivan Bushchik <ivabus@ivabus.dev>
2022-12-11 09:25:45 +0300

    Revert 55775a6c50f3f6f09e67a462bd3ec6fcbe1b8f64

--- tools/relink.sh
+++ tools/relink.sh
@@ -3,10 +3,19 @@
 # script that (re)links all dotfiles during the installation or upgrade.
 # do not run manually
 
-mkdir -p $HOME/.config/ >/dev/null 2>&1
-rm -rf $HOME/`cd $HOME/.dotfiles/dotfiles && find .config -depth -type d | head -n -1` >/dev/null 2>&1
-ln -sv $HOME/.dotfiles/dotfiles/.config/* $HOME/.config/
-for dotfile in `ls -Ap $HOME/.dotfiles/dotfiles/ | grep -E -v /$`
-do
-	ln -sv $HOME/.dotfiles/dotfiles/$dotfile $HOME/$dotfile
-done
+mkdir -p $HOME/.config/nvim $HOME/.config/alacritty $HOME/.config/mako >/dev/null 2>&1
+rm -rf $HOME/.zshrc $HOME/.config/nvim/init.vim $HOME/.config/qt5ct $HOME/.config/alacritty/alacritty.yml $HOME/.config/sway $HOME/.config/waybar $HOME/.config/mako/config  >/dev/null 2>&1
+ln -s $HOME/.dotfiles/configs/zshrc $HOME/.zshrc
+echo ".zshrc linked: $HOME/.dotfiles/configs/zshrc -> $HOME/.zshrc"
+ln -s $HOME/.dotfiles/configs/.profile $HOME/.profile
+echo ".profile linked $HOME/.dotfiles/configs/.profile -> $HOME/.profile"
+ln -s $HOME/.dotfiles/configs/nvim.vim $HOME/.config/nvim/init.vim
+echo "neovim config linked: $HOME/.dotfiles/configs/nvim.vim -> $HOME/.config/nvim/init.vim"
+ln -s $HOME/.dotfiles/configs/alacritty.yml $HOME/.config/alacritty/alacritty.yml
+echo "alacritty config linked: $HOME/.dotfiles/configs/alacritty.yml -> $HOME/.config/alacritty/alacritty.yml"
+
+ln -s $HOME/.dotfiles/configs/sway $HOME/.config/
+ln -s $HOME/.dotfiles/configs/waybar $HOME/.config/
+ln -s $HOME/.dotfiles/configs/mako $HOME/.config/mako/config
+ln -s $HOME/.dotfiles/configs/qt5ct $HOME/.config/qt5ct
+echo "sway (and all other swaywm, wayland stuff things) dotfiles installed"
