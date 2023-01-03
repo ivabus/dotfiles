e079f545810ebc97c88d4c2872359e44a895aa9a
Ivan Bushchik <ivabus@ivabus.dev>
2023-01-03 19:06:00 +0300

    Fix homebrew installation

--- tools/mac.sh
+++ tools/mac.sh
@@ -11,7 +11,7 @@ if [ "$(arch)" == "arm64" ]
 then
 	HOMEBREW_PREFIX="/opt/homebrew"
 else
-	HOMEBREW_PREFIX="/usr/local/homebrew"
+	HOMEBREW_PREFIX="/usr/local"
 fi
 echo "eval \"\$($HOMEBREW_PREFIX/bin/brew shellenv)\"" >> $HOME/.zprofile
 brew install pinentry-mac gpg2
