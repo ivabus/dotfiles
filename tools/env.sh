9364f686ff3424c65b72c4a75091ee3384e01262
Ivan Bushchik <ivabus@ivabus.dev>
2023-01-03 20:00:56 +0300

    Fix env.sh

--- tools/env.sh
+++ tools/env.sh
@@ -1,15 +1,43 @@
 #!/bin/sh
 
-install_compilers() {
+echo "=> This script will create small environment"
+echo "   with dotfiles from ivabus/ivabus-dotfiles"
+echo ":: Press enter for continue"
+read
+
+echo "=> Installing env to ~/.env"
+mkdir -p ~/.env/bin > /dev/null 2>&1
+
+TEA_PREFIX=~/.env/tea
+TEA=~/.env/tea/tea.xyz/v0.18.0/bin/tea
+PATH=$PATH
+
+echo "=> Installing static zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh-bin/master/install)" -q -d ~/.env -e no 
+PATH="$HOME/.env/bin:$PATH"
+
+echo "=> Installing tea"
+zsh -c "TEA_PREFIX=~/.env/tea YES=1 sh <(curl https://tea.xyz)"
+echo "=> Installing neovim"
+$TEA +neovim.io zsh -c "exit"
+echo "=> Installing htop"
+$TEA +htop.dev zsh -c "exit"
+
+
+echo -n ":: Would you like to install compilers/interpretators (LLVM, Rust, Python)? (y/N): "
+read INSTALL_COMPILERS
+if [[ $INSTALL_COMPILERS == "y" || $INSTALL_COMPILERS == "Y" ]]; then
 	echo "=> Installing rust"
 	$TEA +rust-lang.org zsh -c "exit"
 	echo "=> Installing llvm"
 	$TEA +llvm.org zsh -c "exit"
 	echo "=> Installing python"
 	$TEA +python.org zsh -c "exit"
-}
+fi
 
-install_tools() {
+echo -n ":: Would you like to install development tools (git, github cli, make/cmake/meson/ninja)? (y/N): "
+read INSTALL_TOOLS
+if [[ $INSTALL_TOOLS == "y" || $INSTALL_TOOLS == "Y" ]]; then
 	echo "=> Installing git"
 	$TEA +git-scm.org zsh -c "exit"
 	echo "=> Installing GitHub CLI"
@@ -20,42 +48,6 @@ install_tools() {
 	$TEA +cmake.org zsh -c "exit"
 	$TEA +mesonbuild.com zsh -c "exit"
 	$TEA +ninja-build.org zsh -c "exit"
-}
-
-echo "=> This script will create small environment"
-echo "   with dotfiles from ivabus/ivabus-dotfiles"
-echo ":: Press enter for continue"
-read
-
-echo "=> Installing env to ~/.env"
-mkdir -p ~/.env/bin > /dev/null 2>&1
-
-TEA_PREFIX=~/.env/tea
-TEA=~/.env/tea/tea.xyz/v0/bin/tea
-PATH=$PATH
-
-echo "=> Installing static zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh-bin/master/install)" -q -d ~/.env -e no 
-PATH="$HOME/.env/bin:$PATH"
-
-echo "=> Installing tea"
-sh <(curl https://tea.xyz) -s
-echo "=> Installing neovim"
-$TEA +neovim.io zsh -c "exit"
-echo "=> Installing htop"
-$TEA +htop.dev zsh -c "exit"
-
-
-echo -n ":: Would you like to install compilers/interpretators (LLVM, Rust, Python)? (Y/n): "
-read $INSTALL_COMPILERS
-if [ "${INSTALL_COMPILERS^^}" = "Y" ]; then
-	install_compilers
-fi
-
-echo -n ":: Would you like to install development tools (git, github cli, make/cmake/meson/ninja)? (Y/n): "
-read $INSTALL_TOOLS
-if [ "${INSTALL_TOOLS^^}" = "Y" ]; then
-	install_tools
 fi
 
 echo "=> Configuring \$PATH"
@@ -66,10 +58,10 @@ done
 
 echo "=> Installing dotfiles"
 git clone https://github.com/ivabus/ivabus-dotfiles ~/.env/dotfiles
-sh ~/.env/dotfiles/tools/link_env.sh
 ZSH="$HOME/.env/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null 2>&1
 curl -fsSL https://raw.githubusercontent.com/ivabus/ivabus-zsh-theme/master/ivabus.zsh-theme -o $HOME/.env/oh-my-zsh/custom/themes/ivabus.zsh-theme > /dev/null 2>&1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.env/oh-my-zsh/custom/plugins/zsh-syntax-highlighting > /dev/null 2>&1
+sh ~/.env/dotfiles/tools/link_env.sh
 
 echo "=> Environment installed to ~/.env"
 echo "   To start environment just type:"
--- tools/link_env.sh
+++ tools/link_env.sh
@@ -5,7 +5,7 @@ if [ -f ~/.zshrc ]; then
 	echo "=> Moving old zshrc to ~/.zshrc.old"
 	mv ~/.zshrc ~/.zshrc.old
 fi
-ln -s ~/.env/dotfiles/.zshrc ~/.zshrc
+ln -s ~/.env/dotfiles/env/.zshrc ~/.zshrc
 echo "=> Linking neovim files"
 rm -rf ${XDG_CONFIG_HOME:-$HOME/.config}/nvim
 ln -s ~/.env/dotfiles/nvim ${XDG_CONFIG_HOME:-$HOME/.config}/nvim
