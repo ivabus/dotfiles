ca4efb63677b4080d7e7288cce96eb49d85f9604
Ivan Bushchik <ivabus@ivabus.dev>
2023-06-25 15:53:33 +0300

    Update env.sh

--- tools/env.sh
+++ tools/env.sh
@@ -25,17 +25,19 @@ echo "   with dotfiles from ivabus/dotfiles"
 echo "${INPUT} Press enter to continue"
 read
 
+rm -rf $HOME/.env &> /dev/null
+
 if [ $INSTALL_BIN_TOOLS = "1" ]
 then
-echo "${ARROW} Installing env to ~/.env"
-mkdir ~/.env ~/.env/bin > /dev/null 2>&1
+echo "${ARROW} Installing env to $HOME/.env"
+mkdir $HOME/.env $HOME/.env/bin > /dev/null 2>&1
 
-TEA_PREFIX=~/.env/tea
-TEA=~/.env/tea/tea.xyz/v0/bin/tea
+TEA_PREFIX=$HOME/.env/tea
+TEA=$HOME/.env/tea/tea.xyz/v0/bin/tea
 PATH=$PATH
 
 echo "${ARROW} Installing static zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh-bin/master/install)" -q -d ~/.env -e no 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/romkatv/zsh-bin/master/install)" -q -d $HOME/.env -e no 
 PATH="$HOME/.env/bin:$PATH"
 
 echo "${ARROW} Installing tea"
@@ -74,19 +76,19 @@ fi
 echo "${ARROW} Configuring \$PATH"
 for i in "tea" "nvim" "clang" "clang++" "lldb" "rustc" "cargo" "htop" "make" "cmake" "automake" "meson" "ninja" "git" "gh"
 do
-	ln -s $TEA ~/.env/bin/$i
+	ln -s $TEA $HOME/.env/bin/$i
 done
 
 fi
 
 echo "${ARROW} Installing dotfiles"
-$TEA git clone https://github.com/ivabus/dotfiles ~/.env/dotfiles
+$TEA git clone https://github.com/ivabus/dotfiles $HOME/.env/dotfiles
 ZSH="$HOME/.env/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null 2>&1
 curl -fsSL https://raw.githubusercontent.com/ivabus/ivabus-zsh-theme/master/ivabus.zsh-theme -o $HOME/.env/oh-my-zsh/custom/themes/ivabus.zsh-theme > /dev/null 2>&1
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.env/oh-my-zsh/custom/plugins/zsh-syntax-highlighting > /dev/null 2>&1
-sh ~/.env/dotfiles/tools/link_env.sh
+sh $HOME/.env/dotfiles/tools/link_env.sh
 
-echo "${ARROW} Environment installed to ~/.env"
+echo "${ARROW} Environment installed to $HOME/.env"
 if [ $INSTALL_BIN_TOOLS = "1" ]
 then
 echo "   To start environment just type:"
