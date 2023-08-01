37d455fc163548cfbbed68b31bf0f0f2591083f4
Ivan Bushchik <ivabus@ivabus.dev>
2023-08-01 07:03:46 +0300

    Fix install_nix.sh
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- tools/install_nix.sh
+++ tools/install_nix.sh
@@ -4,8 +4,6 @@ set -e
 mkdir $HOME/.config
 ./relink.sh
 mkdir -p $HOME/.dotfiles/zsh/{plugins,themes}
-ZSH="$HOME/.dotfiles/zsh"
-echo "Installing zsh theme and plugins"
 curl -fsSL https://raw.githubusercontent.com/ivabus/ivabus-zsh-theme/master/ivabus.zsh-theme -o $HOME/.dotfiles/zsh/themes/ivabus.zsh-theme
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.dotfiles/zsh/plugins/zsh-syntax-highlighting
 curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
