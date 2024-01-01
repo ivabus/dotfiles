4d868f534578b99dcbf3187263e868e4b72d2fcf
Ivan Bushchik <ivabus@ivabus.dev>
2024-01-01 17:10:43 +0300

    sway/lock.sh: blur screen instead of plain color
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- configs/sway/lock.sh
+++ configs/sway/lock.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 # Times the screen off and puts it to background
+grim /tmp/lockscreen.png && convert -filter Gaussian -resize 20% -blur 0x2.5 -resize 500% /tmp/lockscreen.png /tmp/lockscreen.png
 swayidle \
     timeout 10 'swaymsg "output * dpms off" && brightnessctl -d kbd_backlight set 0%' \
     resume 'swaymsg "output * dpms on"' \
-    before-sleep 'swaylock -c 181924 --font JetBrains\ Mono' &
+    before-sleep 'swaylock -i /tmp/lockscreen --font JetBrains\ Mono' &
 # Locks the screen immediately
-swaylock -c 181924 -s stretch --font JetBrains\ Mono
+swaylock -i /tmp/lockscreen.png -s stretch --font JetBrains\ Mono
 # Kills last background task so idle timer doesn't keep running
 kill %%
+rm /tmp/lockscreen.png
